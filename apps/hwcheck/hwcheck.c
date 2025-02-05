269c0443db90ff884f28775c00642193d21f4fde
Junying Huang <Junyinghuang@users.noreply.github.com>
2025-02-04 18:12:55 -0800

    update flow, bring iic online

--- apps/hwcheck/hwcheck.c
+++ apps/hwcheck/hwcheck.c
@@ -5,6 +5,7 @@
 #include "xstatus.h"
 #include "xil_printf.h"
 #include "sleep.h"
+#include "xiicps.h"
 
 // MIO pinout:
 #define LEDA 7
@@ -74,6 +75,131 @@ void blink(){
 }
 
 
+// Device initialization:
+// I2C:
+#define IIC_DEVICE_ID     XPAR_XIICPS_0_DEVICE_ID
+//#define IIC_DEVICE_ID     XPAR_XIICPS_1_DEVICE_ID
+#define IIC_SCLK_RATE     200000
+XIicPs iicps;
+
+int init_iic(){
+  xil_printf("initializing I2C interface...");
+  XIicPs_Config *cfg = XIicPs_LookupConfig(IIC_DEVICE_ID);
+  if (NULL == cfg) {
+    xil_printf("FAILED.\r\n");
+    return XST_FAILURE;
+  }
+  int status  = XIicPs_CfgInitialize(&iicps, cfg, cfg->BaseAddress);
+  if (status != XST_SUCCESS)  {
+    xil_printf("FAILED.\r\n");
+    return XST_FAILURE;
+  }
+  xil_printf("success.\r\n");
+  xil_printf("performing I2C selftest...");
+  status = XIicPs_SelfTest(&iicps);
+  if (status != XST_SUCCESS)  {
+    xil_printf("FAILED.\r\n");
+    return XST_FAILURE;
+  }
+  xil_printf("success.\r\n");
+  xil_printf("setting IIC clk rate to %d...", IIC_SCLK_RATE);
+  XIicPs_SetSClk(&iicps, IIC_SCLK_RATE);
+  xil_printf("success.\r\n");
+
+  return XST_SUCCESS;
+}
+
+
+// Low-level I2C drivers:
+void iic_send(unsigned addr, unsigned reg){
+  u8 buf = reg;
+
+  xil_printf("I2C:  sending register 0x%x at address 0x%x...", reg, addr);
+  int status = XIicPs_MasterSendPolled
+    (&iicps, &buf, 1, addr);
+  if (status != XST_SUCCESS) {
+    xil_printf("failed.\r\n");
+  } else {
+    xil_printf("success.\r\n");
+  }
+  xil_printf("I2C:  waiting for bus...");
+  while (XIicPs_BusIsBusy(&iicps)) {
+    /* NOP */
+  }
+  xil_printf("done.\r\n");
+}
+
+void iic_set(unsigned addr, unsigned reg, unsigned up, unsigned dn){
+  u8 buf[3];
+  buf[0] = reg;
+  buf[1] = up;
+  buf[2] = dn;
+
+  xil_printf("I2C:  setting register 0x%x to 0x %x %x at address 0x%x...", reg, up, dn, addr);
+  int status = XIicPs_MasterSendPolled
+    (&iicps, buf, 3, addr);
+  if (status != XST_SUCCESS) {
+    xil_printf("failed.\r\n");
+  } else {
+    xil_printf("success.\r\n");
+  }
+  xil_printf("I2C:  waiting for bus...");
+  while (XIicPs_BusIsBusy(&iicps)) {
+    /* NOP */
+  }
+  xil_printf("done.\r\n");
+}
+
+void iic_byte(unsigned addr, unsigned reg, unsigned byte){
+  u8 buf[2];
+  buf[0] = reg;
+  buf[1] = byte;
+
+  xil_printf("I2C:  setting register 0x%x to 0x %x at address 0x%x...", reg, byte, addr);
+  int status = XIicPs_MasterSendPolled
+    (&iicps, buf, 2, addr);
+  if (status != XST_SUCCESS) {
+    xil_printf("failed.\r\n");
+  } else {
+    xil_printf("success.\r\n");
+  }
+  xil_printf("I2C:  waiting for bus...");
+  while (XIicPs_BusIsBusy(&iicps)) {
+    /* NOP */
+  }
+  xil_printf("done.\r\n");
+}
+
+unsigned iic_recv(unsigned addr, unsigned reg, unsigned nbytes){
+  int status;
+  iic_send(addr, reg);
+  u8 buff[nbytes];
+  for (int i=0; i<nbytes; i++){
+    buff[i] = 0;
+  }
+  xil_printf("I2C:  reading back register...");
+  status = XIicPs_MasterRecvPolled
+    (&iicps, buff, nbytes, addr);
+  if (status != XST_SUCCESS) {
+    xil_printf("failed.\r\n");
+  } else {
+    xil_printf("success.\r\n");
+  }
+  xil_printf("I2C:  register:  0x%x value:  ", reg);
+  unsigned value = 0;
+  for (int i=0; i<nbytes; i++){
+    xil_printf("0x%x  ", buff[i]);
+    value = (value<<8) + buff[i];
+  }
+  xil_printf("--> 0x%x (%d)\r\n", value, value);
+  return value;
+}
+
+void check_iic(){
+}
+
+
+
 // these are the addresses for the interfaces as read off from the address editor of the block diagram in vivado
 #define ADDR_AXIL_REGS  0x40000000
 
@@ -103,6 +229,7 @@ int main(){
   xil_printf("Trenz Eval Board Hardware Testing (Development)\r\n");
   int status = 0;
   status |= init_gpiops();
+  status |= init_iic();  
   if (status != XST_SUCCESS) {
     xil_printf("Hardware initialization has FAILED.\r\n");
     return 0;
